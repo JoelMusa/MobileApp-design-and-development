c3e81482cd53cba4a2895bcbb838f42bc1b75783
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-09-29 09:18:07 +0300

    Ionic setup

---
+++ simple-navigation-app-labwork-2/android/app/src/main/java/simple/navigation/app/labwork/v2/MainActivity.java
@@ -0,0 +1,5 @@
+package simple.navigation.app.labwork.v2;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ simple-navigation-app-labwork-2/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'simple.navigation.app.labwork.v2',
+  appName: 'simple-navigation-app-labwork-2',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
