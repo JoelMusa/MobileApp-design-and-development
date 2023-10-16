3390029e9b8dfbaeb28978e706a8f75a86c9bc70
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-10-16 15:04:13 +0300

    Labwork 3

---
+++ Labwork3-task-2/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Labwork3-task-2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
